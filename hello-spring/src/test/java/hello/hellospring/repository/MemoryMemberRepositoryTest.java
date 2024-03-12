1a5845445bacdd999e10475004ccfc39c05e6e41
eoeo2255 <naiga6@gmail.com>
2024-03-12 23:50:26 +0900

    테스트케이스 작성

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -4,6 +4,8 @@ import hello.hellospring.domain.Member;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemoryMemberRepositoryTest {
@@ -18,13 +20,38 @@ public class MemoryMemberRepositoryTest {
     @Test
     public void save(){
         Member member = new Member();
-        member.setName("eoeo");
+        member.setName("하나");
 
         repository.save(member);
         Member result = repository.findById(member.getId()).get();
         assertThat(member).isEqualTo(result);
     }
 
+    @Test
+    public void findByName(){
+        Member member = new Member();
+        member.setName("둘");
+        repository.save(member);
+
+        Member result = repository.findByName("둘").get();
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("하나");
+
+        Member member2 = new Member();
+        member2.setName("둘");
+
+        repository.save(member1);
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
 
 
 }
